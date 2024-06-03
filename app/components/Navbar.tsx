cfeb4f6a10176f3e00fa9cd56dbf60120096a9f9
arrovain <arrovain@gmail.com>
2024-06-03 15:44:11 +0300

    navbar...

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,3 +1,7 @@
 export function Navbar() {
-  return <div></div>;
+  return (
+    <nav className="relative max-w-7xl w-full flex md:grid md:grid-cols-12 items-center px-4 md:px-8 mx-auto py-7">
+      <div className="md:col-span-3"></div>
+    </nav>
+  );
 }
