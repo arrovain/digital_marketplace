7f20bc5a3330252ec94f036f194722c007122094
arrovain <arrovain@gmail.com>
2024-06-03 16:32:41 +0300

    button, menu

--- app/components/MobileMenu.tsx
+++ app/components/MobileMenu.tsx
@@ -1,5 +1,16 @@
-import { Sheet } from "@/components/ui/sheet";
+import { Sheet, SheetTrigger } from "@/components/ui/sheet";
 
 export function MobileMenu() {
-  return <Sheet></Sheet>;
+  return (
+    <Sheet>
+        <SheetTrigger asChild>
+            <Button>
+                <Menu className="w-4 h-4"/>
+            </Button>
+
+        </SheetTrigger>
+    </Sheet>;
+  ),
+  
+  
 }
