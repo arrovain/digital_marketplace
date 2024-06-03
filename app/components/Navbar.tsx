86cf65e9b6b14392b88bf5f10c4b78c462202b2b
arrovain <arrovain@gmail.com>
2024-06-03 15:51:18 +0300

    NavbarLinks

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,12 +1,16 @@
 import Link from "next/link";
+import { NavbarLinks } from "./NavbarLinks";
 
 export function Navbar() {
   return (
     <nav className="relative max-w-7xl w-full flex md:grid md:grid-cols-12 items-center px-4 md:px-8 mx-auto py-7">
       <div className="md:col-span-3">
         <Link href="">
-          <h1>ArrovainUI</h1>
+          <h1 className="text-2xl font-semibold">
+            Arrovain<span className="text-violet-500">UI</span>
+          </h1>
         </Link>
+        <NavbarLinks />
       </div>
     </nav>
   );
