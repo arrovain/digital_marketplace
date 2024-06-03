64d991eb2452ba2e8767babc11ce28a72d35854c
arrovain <arrovain@gmail.com>
2024-06-03 16:24:29 +0300

    bg-muted : hover: code

--- app/components/NavbarLinks.tsx
+++ app/components/NavbarLinks.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
 
 export const navbarLinks = [
   {
@@ -30,7 +31,16 @@ export function NavbarLinks() {
   return (
     <div className="hidden md:flex justify-center items-center col-span-6 gap-x-2">
       {navbarLinks.map((item) => (
-        <Link href={item.href} key={item.id} className="">
+        <Link
+          href={item.href}
+          key={item.id}
+          className={cn(
+            location === item.href
+              ? "bg-muted"
+              : "hover:bg-muted hover:bg-opacity-75",
+            "group flex items-center px-2 py-2 font-medium rounded-md"
+          )}
+        >
           {item.name}
         </Link>
       ))}
