a765d61d263486de9c3f944f5a471ff84c8a954e
arrovain <arrovain@gmail.com>
2024-06-03 16:42:30 +0300

    location pathname and cn

--- app/components/MobileMenu.tsx
+++ app/components/MobileMenu.tsx
@@ -1,12 +1,14 @@
 "use client";
-
+import { cn } from "@/lib/utils";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Button } from "@/components/ui/button";
 import { Menu } from "lucide-react";
 import { navbarLinks } from "./NavbarLinks";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export function MobileMenu() {
+  const location = usePathname();
   return (
     <Sheet>
       <SheetTrigger asChild>
@@ -17,7 +19,16 @@ export function MobileMenu() {
       <SheetContent>
         <div className="mt-5 flex px-2 space-y-1 flex-col">
           {navbarLinks.map((item) => (
-            <Link href={item.href} key={item.id}>
+            <Link
+              href={item.href}
+              key={item.id}
+              className={cn(
+                location === item.href
+                  ? "bg-muted"
+                  : "hover:bg-muted hover:bg-opacity-75",
+                "group flex items-center px-2 py-2 font-medium rounded-md"
+              )}
+            >
               {item.name}
             </Link>
           ))}
