05f9bb11b0d39c3ef7a37139208fe8ca04b12171
arrovain <arrovain@gmail.com>
2024-06-03 16:39:51 +0300

    mobilemenu mapping

--- app/components/MobileMenu.tsx
+++ app/components/MobileMenu.tsx
@@ -1,16 +1,28 @@
-import { Sheet, SheetTrigger } from "@/components/ui/sheet";
+"use client";
+
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import { Menu } from "lucide-react";
+import { navbarLinks } from "./NavbarLinks";
+import Link from "next/link";
 
 export function MobileMenu() {
   return (
     <Sheet>
-        <SheetTrigger asChild>
-            <Button>
-                <Menu className="w-4 h-4"/>
-            </Button>
-
-        </SheetTrigger>
-    </Sheet>;
-  ),
-  
-  
+      <SheetTrigger asChild>
+        <Button variant={"outline"} size={"icon"}>
+          <Menu className="w-4 h-4" />
+        </Button>
+      </SheetTrigger>
+      <SheetContent>
+        <div className="mt-5 flex px-2 space-y-1 flex-col">
+          {navbarLinks.map((item) => (
+            <Link href={item.href} key={item.id}>
+              {item.name}
+            </Link>
+          ))}
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
 }
