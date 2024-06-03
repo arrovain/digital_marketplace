7b2e2e5c8ed7ed9da0ded91aefde2ec22864e765
arrovain <arrovain@gmail.com>
2024-06-03 15:53:55 +0300

    array of navbarlinks

--- app/components/NavbarLinks.tsx
+++ app/components/NavbarLinks.tsx
@@ -1,3 +1,26 @@
+export const navbarLinks = [
+  {
+    id: 0,
+    name: "Home",
+    href: "/",
+  },
+  {
+    id: 1,
+    name: "Templates",
+    href: "#",
+  },
+  {
+    id: 2,
+    name: "UI Kits",
+    href: "#",
+  },
+  {
+    id: 3,
+    name: "Icons",
+    href: "#",
+  },
+];
+
 export function NavbarLinks() {
   return <div></div>;
 }
