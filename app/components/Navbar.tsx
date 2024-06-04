4d8f6f36b5a53e3074ff7f743ad59ef161d67a52
arrovain <arrovain@gmail.com>
2024-06-04 18:57:00 +0300

    getUser

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,8 +6,11 @@ import {
   LoginLink,
   RegisterLink,
 } from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-export function Navbar() {
+export async function Navbar() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
   return (
     <nav className="relative max-w-7xl w-full flex md:grid md:grid-cols-12 items-center px-4 md:px-8 mx-auto py-7">
       <div className="md:col-span-3">
