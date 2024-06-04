382b16a3daf1a797b124df230a7709f831fdac40
arrovain <arrovain@gmail.com>
2024-06-04 19:18:44 +0300

    dropdown-menu-items for links

--- app/components/UserNav.tsx
+++ app/components/UserNav.tsx
@@ -3,7 +3,10 @@ import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
   DropdownMenuLabel,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
@@ -20,9 +23,24 @@ export function UserNav() {
       <DropdownMenuContent className="w-56" align="end" forceMount>
         <DropdownMenuLabel className="font-normal">
           <div className="flex flex-col space-y-1">
-            <p>icme</p>
+            <p
+              className="text-sm font-medium leading-none
+            "
+            >
+              icme
+            </p>
+            <p className="text-xs leading-none text-muted-foreground ">
+              icmemahmut@gmail.com
+            </p>
           </div>
         </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem>test</DropdownMenuItem>
+          <DropdownMenuItem>test</DropdownMenuItem>
+          <DropdownMenuItem>test</DropdownMenuItem>
+          <DropdownMenuItem>test</DropdownMenuItem>
+        </DropdownMenuGroup>
       </DropdownMenuContent>
     </DropdownMenu>
   );
